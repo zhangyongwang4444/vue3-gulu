15883818e63e073e42da0ed37bb825d0c09454a9
zyw <20225614@qq.com>
2020-08-28 12:19:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi zyw</div>
+  <Zyw/>
+</template>
+
+<script>
+import Zyw from './components/Zyw.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Zyw
+  }
+}
+</script>
---
+++ src/components/Zyw.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我的第一个组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Zyw"
+}
+</script>
+
+<style scoped>
+
+</style>
